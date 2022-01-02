fe5c5528c872c3b87467675059347f6751739341
yoon-junseo <ysj6844@naver.com>
2022-01-02 20:09:51 +0900

    feat: add LinkedList
    1. 양방향 연결 리스트

---
+++ DataStructures/6장 - 연결리스트/linkedList.js
@@ -0,0 +1,102 @@
+function Node(element) {
+  this.element = element;
+  this.next = null;
+  this.previous = null;
+}
+
+function LinkedList() {
+  this.head = new Node("head");
+  this.find = find;
+  this.insert = insert;
+  this.findPrevious = findPrevious;
+  this.remove = remove;
+  this.findLast = findLast;
+  this.dispReverse = dispReverse;
+  this.display = display;
+}
+
+function find(item) {
+  let currNode = this.head;
+
+  while (currNode.element !== item) {
+    currNode = currNode.next;
+  }
+
+  return currNode;
+}
+
+function insert(newElement, item) {
+  let newNode = new Node(newElement);
+  let current = this.find(item);
+
+  newNode.next = current.next;
+  newNode.previous = current;
+  current.next = newNode;
+}
+
+function findPrevious(item) {
+  let currNode = this.head;
+
+  while (currNode.next !== null && currNode.next.element !== item) {
+    currNode = currNode.next;
+  }
+
+  return currNode;
+}
+
+function remove(item) {
+  let currNode = this.find(item);
+
+  if (currNode.next !== null) {
+    currNode.previous.next = currNode.next;
+    currNode.next.previous = currNode.previous;
+    currNode.next = null;
+    currNode.previous = null;
+  }
+}
+
+function findLast() {
+  let currNode = this.head;
+
+  while (currNode.next !== null) {
+    currNode = currNode.next;
+  }
+
+  return currNode;
+}
+
+function dispReverse() {
+  let currNode = this.head;
+  currNode = this.findLast();
+
+  while (currNode.previous !== null) {
+    console.log(currNode.element);
+    currNode = currNode.previous;
+  }
+}
+
+function display() {
+  let currNode = this.head;
+
+  while (currNode.next !== null) {
+    console.log(currNode.next.element);
+    currNode = currNode.next;
+  }
+}
+
+// /**
+//  * 예제 코드
+//  */
+let cities = new LinkedList();
+cities.insert("1", "head");
+cities.insert("2", "1");
+cities.insert("3", "2");
+cities.insert("4", "3");
+cities.display();
+console.log("---------------");
+
+cities.remove("4");
+cities.display();
+console.log("---------------");
+
+cities.dispReverse();
